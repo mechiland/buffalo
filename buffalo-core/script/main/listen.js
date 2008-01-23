ebc5cbe00bef8eb6c1b81eac423bd0a89b8d3e96
Michael Chen <mechiland@gmail.com>
2008-01-23 15:13:25 +0000

    add buffalo listen module

---
+++ buffalo-core/script/main/listen.js
@@ -0,0 +1,12 @@
+Buffalo.Listen = Class.create();
+Buffalo.Listen.prototype = {
+    
+    initialize: function(buffaloObj) {
+        this.buffalo = buffaloObj;
+    }, 
+    
+    listen: function(service, parameters, action) {
+        
+    }
+    
+}
